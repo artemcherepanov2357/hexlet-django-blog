8ecadcc63547b5fad08056e627ceb113f2bab042
Artem Cherepanov <artemcherepanov2357@gmail.com>
2026-08-13 05:25:45 +0300

    articles2

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -5,4 +5,8 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("article")
+    context = {
+        'app_name': 'Блог о Django',  # Название приложения
+        'articles': []  # Пока пустой список, позже добавите статьи из базы данных
+    }
+    return render(request, 'articles/index.html', context)
